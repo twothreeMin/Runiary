9caf64aa05810e590a6cd2ff6bb9bf506ec17dbe
이상민 <foreverything1023@gmail.com>
2022-04-26 20:33:19 +0900

    [modify: component(css)] 반응형 웹 크기 조정

--- src/components/list/RunItem.js
+++ src/components/list/RunItem.js
@@ -3,17 +3,19 @@ import './RunItem.css';
 const RunItem = () => {
   return (
     <div className="RunItem">
-      <div className="RunItem__date">
-        2022.04.22
-      </div>
-      <div className="RunItem__dis-time">
-        4km / 23:11
-      </div>
-      <div className="RunItem__pace">
-        5:50
-      </div>
-      <div className="RunItem__condition">
-        😆
+      <div className="RunItem__information">
+        <div className="information__date">
+          2022.04.22
+        </div>
+        <div className="information__dis-time">
+          4km / 23:11
+        </div>
+        <div className="information__pace">
+          5:50
+        </div>
+        <div className="information__condition">
+          😆
+        </div>
       </div>
     </div>
   );
