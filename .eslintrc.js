e83ffa3275c44aee563c39e5eb15e7f2d217dafa
이상민 <foreverything1023@gmail.com>
2022-04-28 19:48:30 +0900

    [config]: prettier 설치

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,32 +4,29 @@ module.exports = {
     es6: true,
     node: true,
   },
+  plugins: ['prettier'],
   extends: [
     'airbnb',
     'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
   ],
   rules: {
+    'prettier/prettier': 'error',
     'arrow-body-style': ['error', 'always'],
     'object-curly-newline': ['error', { multiline: true }],
-    'react/jsx-filename-extension':
-      ['error',
-        { extensions: ['.js', '.jsx'] },
-      ],
-    'react/jsx-one-expression-per-line':
-      [0,
-        { allow: 'none' },
-      ],
-    'react/function-component-definition':
-      ['warn',
-        { namedComponents: 'arrow-function' },
-      ],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'react/jsx-one-expression-per-line': [0, { allow: 'none' }],
+    'react/function-component-definition': [
+      'warn',
+      { namedComponents: 'arrow-function' },
+    ],
     'no-unused-vars': 'off',
     'import/prefer-default-export': 'off',
     'jsx-a11y/label-has-associated-control': 'off',
-    'jsx-a11y/no-static-element-interactions':
-      ['warn',
-        { handlers: ['onClick', 'onKeyUp'] },
-      ],
+    'jsx-a11y/no-static-element-interactions': [
+      'warn',
+      { handlers: ['onClick', 'onKeyUp'] },
+    ],
     'no-console': 'off',
   },
 };
