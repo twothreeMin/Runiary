e54b093b25b068f66e9f74f42c62dabb04d0ae1e
이상민 <foreverything1023@gmail.com>
2022-05-08 23:50:07 +0900

    [add: component] Modal 창 추가(미완성) 및 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
 import './App.css';
 
-// import { RuniaryForm } from './components/form/RunDiaryForm';
 import { useState } from 'react';
 import { RuniaryHeader } from './components/header/RuniaryHeader';
 import { RunItemButton } from './components/list/RunItemButton';
 import { RunItems } from './components/list/RunItems';
 import { Modal } from './components/form/Modal';
 
-// const dummyRunInfoList = [
-//   {
-//     id: 1,
-//     date: '2022.04.25',
-//     distance: 4,
-//     time: '22:32',
-//     pace: '05:50',
-//     condition: '😆',
-//     feeling: '오늘은 너무 가벼웠다.',
-//   },
-//   {
-//     id: 2,
-//     date: '2022.04.24',
-//     distance: 1,
-//     time: '05:11',
-//     pace: '05:11',
-//     condition: '😆',
-//     feeling: 'good!',
-//   },
-//   {
-//     id: 3,
-//     date: '2022.04.23',
-//     distance: 3,
-//     time: '15:50',
-//     pace: '05:40',
-//     condition: '🥵',
-//     feeling:
-//       '초반에 컨디션 좋은줄 알고 무리한 페이스로 달리다가 결국 뒤로가서 지쳐버림.',
-//   },
-// ];
 const App = () => {
   const [data, setData] = useState([]);
   const [open, setOpen] = useState();
@@ -51,18 +20,27 @@ const App = () => {
     const newRunData = {
       ...runData,
       pace,
+      id: Math.random().toString(),
     };
     setData([newRunData, ...data]);
   };
 
-  const onClickOpenModal = (openModal) => {
-    setOpen(openModal);
+  const onClickOpenModal = () => {
+    setOpen(true);
+  };
+
+  const onClickCloseModal = () => {
+    setOpen(false);
   };
 
   return (
     <div className="App">
       <RuniaryHeader />
-      <Modal onCreate={onCreate} openModal={open} />
+      <Modal
+        onCreate={onCreate}
+        onClickCloseModal={onClickCloseModal}
+        openModal={open}
+      />
       <RunItemButton onClickOpenModal={onClickOpenModal} />
       <RunItems runInfoList={data} />
     </div>
--- src/components/form/Modal.js
+++ src/components/form/Modal.js
@@ -1,26 +1,119 @@
 import PropTypes from 'prop-types';
-import { RuniaryForm } from './RuniaryForm';
+import React, { useRef, useState } from 'react';
 
-export const Modal = ({ onCreate, openModal }) => {
-  const saveRuniaryDataHandler = (enteredRuniaryData) => {
+import './Modal.css';
+
+export const Modal = ({ onCreate, onClickCloseModal, openModal }) => {
+  const timeInput = useRef();
+  const [enteredDistance, setEnteredDistance] = useState('');
+  const [enteredTime, setEnteredTime] = useState('00:00:00');
+  const [enteredFeeling, setEnteredFeeling] = useState('');
+  const [enteredCondition, setEnteredCondition] = useState('');
+
+  const distanceChangeHandler = (e) => {
+    setEnteredDistance(e.target.value);
+  };
+
+  const timeChangeHandler = (e) => {
+    setEnteredTime(e.target.value);
+  };
+
+  const feelingChangeHandler = (e) => {
+    setEnteredFeeling(e.target.value);
+  };
+
+  const conditionChangeHandler = (e) => {
+    setEnteredCondition(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const regex = /\d{2}:\d{2}:\d{2}/;
+
+    if (!regex.test(enteredTime)) {
+      alert('time form 양식에 어긋납니다.');
+      timeInput.current.style.borderColor = 'red';
+      setEnteredTime('00:00:00');
+      return;
+    }
+
     const runiaryData = {
-      ...enteredRuniaryData,
-      id: Math.random().toString(),
+      distance: enteredDistance,
+      time: enteredTime,
+      feeling: enteredFeeling,
+      condition: enteredCondition,
     };
 
     onCreate(runiaryData);
+    setEnteredDistance('');
+    setEnteredTime('00:00:00');
+    setEnteredFeeling('');
+    setEnteredCondition('1');
+  };
+
+  const onClickHandler = () => {
+    onClickCloseModal();
   };
 
   return (
     <div className={openModal ? 'modal openModal' : 'modal'}>
-      {openModal ? (
-        <RuniaryForm onSaveRuniaryData={saveRuniaryDataHandler} />
-      ) : null}
+      <form onSubmit={submitHandler}>
+        <div className="RunDiaryForm__condition">
+          <label htmlFor="condition">Today Condition</label>
+          <select
+            id="condition"
+            value={enteredCondition}
+            onChange={conditionChangeHandler}
+          >
+            <option value="No running"> No running </option>
+            <option value="😆"> 😆 </option>
+            <option value="😀"> 😀 </option>
+            <option value="🙂"> 🙂 </option>
+            <option value="😨 "> 😨 </option>
+            <option value="🥵"> 🥵 </option>
+          </select>
+        </div>
+        <div className="RunDiaryForm__run-distance">
+          <label htmlFor="run-distance">Distance</label>
+          <input
+            type="text"
+            placeholder="Ex) 3"
+            value={enteredDistance}
+            onChange={distanceChangeHandler}
+          />
+        </div>
+        <div className="RunDiaryForm__run-time">
+          <label htmlFor="run-time">Time</label>
+          <input
+            type="text"
+            ref={timeInput}
+            placeholder="Ex) 00:15:30"
+            value={enteredTime}
+            onChange={timeChangeHandler}
+          />
+        </div>
+        <div className="RunDiaryForm__run-feeling">
+          <label htmlFor="run-feeling">How are you feeling today?</label>
+          <textarea
+            name="feeling"
+            cols="30"
+            rows="10"
+            value={enteredFeeling}
+            onChange={feelingChangeHandler}
+          />
+        </div>
+        <button type="submit">등록</button>
+        <button type="button" onClick={onClickHandler}>
+          닫기
+        </button>
+      </form>
     </div>
   );
 };
 
 Modal.propTypes = {
   onCreate: PropTypes.func.isRequired,
+  onClickCloseModal: PropTypes.func.isRequired,
   openModal: PropTypes.bool.isRequired,
 };
