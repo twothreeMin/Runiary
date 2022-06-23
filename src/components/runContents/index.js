85228d0b881d09e9a6e0c0582b80344f50c67c2e
이상민 <foreverything1023@gmail.com>
2022-06-23 23:10:52 +0900

    [add] runItem Date 추가

--- src/components/runContents/index.js
+++ src/components/runContents/index.js
@@ -2,25 +2,35 @@ import { useEffect, useState } from 'react';
 import { AddRunItem } from './addRunItem';
 import { RunItem } from './runItem';
 
+const initLocalData = () => {
+  if (!localStorage.getItem('runiary'))
+    localStorage.setItem('runiary', JSON.stringify([]));
+};
+
+const getRunTime = (data) => {
+  const timeSplit = data.time.split(':');
+  const seconds = +timeSplit[0] * 60 + +timeSplit[1] + +timeSplit[2] / 60;
+  const paceMinutes = seconds / data.distance;
+
+  return (Math.round((paceMinutes + Number.EPSILON) * 100) / 100).toString();
+};
+
 export const RunContents = () => {
+  initLocalData();
   const [runData, setRunData] = useState(
     JSON.parse(localStorage.getItem('runiary')),
   );
 
   useEffect(() => {
-    console.log('Update!');
     localStorage.setItem('runiary', JSON.stringify(runData));
   }, [runData]);
 
   const onAddingRunItem = (runningData) => {
-    const timeSplit = runningData.time.split(':');
-    const seconds = +timeSplit[0] * 60 + +timeSplit[1] + +timeSplit[2] / 60;
-    const paceMinutes = seconds / runningData.distance;
-    const pace = (
-      Math.round((paceMinutes + Number.EPSILON) * 100) / 100
-    ).toString();
+    const date = new Date().toLocaleDateString('ko-KR');
+    const pace = getRunTime(runningData);
     const newRunData = {
       ...runningData,
+      date,
       pace,
       id: Math.random().toString(),
     };
